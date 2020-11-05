daf97e395c85bd241c01933eeda396cf0c88f159
mhy1264 <s710516@student2.ylsh.chc.edu.tw>
2020-11-05 22:35:26 +0800

    Rewrite

---
+++ show/組合/show.cpp
@@ -0,0 +1,69 @@
+/*
+7 6
+16 2
+36 1
+85 2
+23 2
+30 1
+66 2*/
+#include<bits/stdc++.h>
+using namespace std;
+void binary(int situation[],int N)
+{
+    int j;
+    situation[0]++;
+    for(j=0;j<N;j++)
+    {
+        if(situation[j]==2)
+        {
+            situation[j+1]++;
+            situation[j]=0;
+        }
+    }
+}
+int main()
+{
+    int a,N,i,j,cpop=0,cbud=0,mpop=0,mbud;
+//    cin>>a;
+    cin>>N;
+    pair <int,int> team[i]; // pop bud
+    int situation[N];
+    mbud=a;
+    for(i=0;i<N;i++)
+    {
+        situation[i]=0;
+    }
+    /*
+    for(i=0;i<N;i++)
+    {
+        cin>>team[i].first>>team[i].second;
+    }
+*/
+    for(i=0;i<pow(N,2);i++)
+    {
+        cout<<i<<"\t";
+        binary(situation,N);
+        for(j=0;j<N;j++)
+        {
+            cout<<situation[j];
+        }
+        cout<<endl;
+        /*
+        for(j=0;j<N;j++)
+        {
+            cpop+=situation[j]*team[j].first;
+            cbud+=situation[j]*team[j].second;
+        }
+        cout<<cpop<<" "<<cbud<<endl;
+        if(cpop>mpop && a>cbud)
+        {
+            cout<<"===============>YES"<<endl;
+            mbud=cbud;
+            mpop=cpop;
+        }
+        cpop=0;
+        cbud=0;
+*/
+    }
+  //  cout<<mpop<<" "<<mbud;
+}
