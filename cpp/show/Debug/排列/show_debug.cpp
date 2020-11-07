06ff54bed89eee58e8ac352d0008d09d05b1db5c
mhy1264 <s710516@student2.ylsh.chc.edu.tw>
2020-11-07 22:45:15 +0800

    add show_debud

--- cpp/show/Debug/排列/show_debug.cpp
+++ cpp/show/Debug/排列/show_debug.cpp
@@ -21,26 +21,27 @@ void show(int number[],int N)
 void add(int number[],int N)
 {
     number[0]++;
-    show(number,N);
     int i;
     for(i=0;i<=N;i++)
     {
-        if(number[i]==N)
+        if(number[i]>N)
         {
             number[i]=0;
             number[i+1]++;
         }
     }
-    show(number,N);
 }
 
 int check_repeat(int number[],int N)
 {
+    cout<<"here=>>";
     int i,compose[N],mup;
+    reset(compose,N);
     for(i=0;i<N;i++)
     {
         compose[number[i]]++;
     }
+    show(compose,N);
     for(i=0;i<N;i++)
     {
         mup*=compose[i];
@@ -61,34 +62,20 @@ int main()
     cin>>a>>N;
     pair<int,int> team[i];
     int number[N+1];
-    for(i=0;i<N;i++)
-    {
-        cin>>team[i].first>>team[i].second;
-    }
+ //   for(i=0;i<N;i++)
+ //   {
+ //       cin>>team[i].first>>team[i].second;
+ //   }
     reset(number,N);
     while(number[N]==0)
     {
+        show(number,N);
         add(number,N);
-  //      show(number,N);
         if(check_repeat(number,N))
         {
-            if(team[i].second+cbud<a)
-            {
-                cpop+=team[i].first;
-                cbud+=team[i].second;
-            }
-            else
-            {
-                if(cpop<mpop)
-                {
-                    mpop=cpop;
-                    tbud=cbud;
-                }
-                cpop=0;
-                cbud=0;
-            }
+            cout<<"Yes";
+            show(number,N);
         }
     }
-    cout<<cpop<<" "<<tbud<<endl;
 
 }
