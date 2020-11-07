0ce359c96c629bf32464247a41266f4031d98968
mhy1264 <s710516@student2.ylsh.chc.edu.tw>
2020-11-07 14:30:23 +0800

    rewrite show(組合)

--- cpp/show/排列/show.cpp
+++ cpp/show/排列/show.cpp
@@ -1,60 +1,57 @@
-/*
-7 6
-16 2
-36 1
-85 2
-23 2
-30 1
-66 2
-160 7
-*/
 #include<bits/stdc++.h>
 using namespace std;
+void reset(int number[],int N)
+{
+    int i;
+    for(i=0;i<=N;i++)
+    {
+        number[i]=0;
+    }
+}
+
+void show(int number[],int N)
+{
+    int i;
+    for(i=0;i<=N;i++)
+    {
+        cout<<number[i]<<" ";
+    }
+    cout<<endl;
+}
+void add(int number[],int N)
+{
+    number[0]++;
+    show(number,N);
+    int i;
+    for(i=0;i<=N;i++)
+    {
+        if(number[i]==6)
+        {
+            number[i]=0;
+            number[i+1]++;
+        }
+    }
+}
+
+int check_repeat(int number,int N)
+{
+
+}
 int main()
 {
     int a,N,i,j;
-    int cbudget=0,finalbudget=0,cpop=0,maxp=0;
     cin>>a>>N;
-    pair<int ,int> team[N];  //first=> population second=>budget
-    for(i=0;i<N;i++)
+    pair<int,int> team[i];
+    int number[N+1];
+    reset(number,N);
+    while(number[N]==0)
     {
-        cin>>team[i].first>>team[i].second;
+        add(number,N);
+        show(number,N);
+        if(check_repeat(number,N))
+
+        cout<<"finish";
+        break;
     }
 
-    for(i=0;i<N;i++)
-    {
-        if(cbudget+team[i].second<=a)
-        {
-            cbudget+=team[i].second;
-            cpop+=team[i].first;
-        }
-    }
-    if(cpop>maxp)
-    {
-        maxp=cpop;
-        finalbudget=cbudget;
-    }
-    cout<<maxp<<" "<<finalbudget<<endl;
-    while(next_permutation(team,team+N))
-    {
-        for(i=0;i<N;i++)
-        {
-            if(cbudget+team[i].second<=a)
-            {
-                cbudget+=team[i].second;
-                cpop+=team[i].first;
-            }
-            else
-            {
-                if(cpop>maxp)
-                {
-                    maxp=cpop;
-                    finalbudget=cbudget;
-                    cout<<maxp<<" "<<finalbudget<<endl;
-                }
-            }
-            cbudget=0;
-            cpop=0;
-        }
-    }
 }
